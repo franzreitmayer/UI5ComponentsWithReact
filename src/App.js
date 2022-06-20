24cd6b98543f71aece98c9f3d6ccac435ad5befa
Franz Reitmayer <franz.reitmayer@reitmayer.eu>
2022-06-20 18:57:28 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -3,27 +3,30 @@ import {
   FlexBoxAlignItems,
   FlexBoxDirection,
   FlexBoxJustifyContent,
-  Link,
-  LinkDesign,
-  ShellBar
+  ShellBar, ThemeProvider
 } from '@ui5/webcomponents-react';
 import './App.css';
+import { MyApp } from "./MyApp";
+import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <ShellBar primaryTitle="UI5 Web Components for React Template" />
-      <FlexBox
-        style={{ width: '100%', height: '100vh' }}
-        direction={FlexBoxDirection.Column}
-        justifyContent={FlexBoxJustifyContent.Center}
-        alignItems={FlexBoxAlignItems.Center}
-      >
-        <Link href="https://sap.github.io/ui5-webcomponents-react/" target="_blank" design={LinkDesign.Emphasized}>
-          Getting Started with UI5 Web Component for React
-        </Link>
-      </FlexBox>
-    </>
+    /*     <>
+          <ShellBar primaryTitle="UI5 Web Components for React Template" />
+          <FlexBox
+            style={{ width: '100%', height: '100vh' }}
+            direction={FlexBoxDirection.Column}
+            justifyContent={FlexBoxJustifyContent.Center}
+            alignItems={FlexBoxAlignItems.Center}
+          >
+            <MyApp />
+          </FlexBox>
+        </> */
+    <HashRouter>
+      <ThemeProvider>
+        <MyApp />
+      </ThemeProvider>
+    </HashRouter>
   );
 }
 
